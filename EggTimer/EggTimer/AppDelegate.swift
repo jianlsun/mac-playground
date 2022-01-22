ef4121595a6bf3274cb12cb278397f7989940c33
jianlsun <jianlsun@cisco.com>
2022-01-22 22:34:15 +0800

    feat: add EggTimer main window UI

---
+++ EggTimer/EggTimer/AppDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.swift
+//  EggTimer
+//
+//  Created by jianlsun on 2022/1/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+    @IBOutlet weak var startTimerMenuItem: NSMenuItem!
+    @IBOutlet weak var stopTimerMenuItem: NSMenuItem!
+    @IBOutlet weak var resetTimerMenuItem: NSMenuItem!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ EggTimer/EggTimer/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by jianlsun on 2022/1/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    @IBOutlet weak var timeLeftField: NSTextField!
+    
+    @IBOutlet weak var eggImage: NSImageView!
+    @IBOutlet weak var startButton: NSButton!
+    @IBOutlet weak var stopButton: NSButton!
+    @IBOutlet weak var resetButton: NSButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+    @IBAction func startButtonClicked(_ sender: Any) {
+    }
+    
+    @IBAction func stopButtonClicked(_ sender: Any) {
+    }
+    
+    @IBAction func resetButtonClicked(_ sender: Any) {
+    }
+    
+    // MARK: - IBActions menu
+    
+    @IBAction func startTimerMenuItemSelected(_ sender: Any) {
+        startButtonClicked(sender)
+    }
+    
+    @IBAction func stopTimerMenuItemSelected(_ sender: Any) {
+        stopButtonClicked(sender)
+    }
+    
+    @IBAction func resetTimerMenuItemSelected(_ sender: Any) {
+        resetButtonClicked(sender)
+    }
+}
+
